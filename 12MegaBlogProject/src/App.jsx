d400d988288718914f2beb4ad859c5716bf20058
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-23 17:42:52 +0530

    some new addition to the jsx

--- 12MegaBlogProject/src/App.jsx
+++ 12MegaBlogProject/src/App.jsx
@@ -1,16 +1,43 @@
+import { useEffect, useState } from "react"
 import conf from "./conf/conf.js"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js"
+import { Outlet } from "react-router-dom"
+
 
 
 function App() {
-  console.log(conf.appwriteUrl, conf.appwriteBucketId)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>
-        A blog app with appwrite
-      </h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {
+          dispatch(logout());
+        }
+
+      }).finally(() => setLoading(false))
+  })
+
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
