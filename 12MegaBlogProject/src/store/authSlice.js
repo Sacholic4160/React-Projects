eb6b42f8b0585229e96988bcab81d45d86261050
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-27 12:07:53 +0530

    the file for slices of stores

--- 12MegaBlogProject/src/store/authSlice.js
+++ 12MegaBlogProject/src/store/authSlice.js
@@ -1,26 +1,25 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-let initialState = {
-    status: false,
+const initialState = {
+    status : false,
     userData: null
 }
 
 const authSlice = createSlice({
-    name: 'auth',
+    name: "auth",
     initialState,
     reducers: {
         login: (state, action) => {
             state.status = true;
-            state.userData = action.payload.userData
+            state.userData = action.payload.userData;
         },
-        logout: (state, action) => {
+        logout: (state) => {
             state.status = false;
-            state.userData = null
+            state.userData = null;
         }
-    }
+     }
 })
 
+export const {login, logout} = authSlice.actions;
 
-
-export const { login, logout} = authSlice.actions;
 export default authSlice.reducer;
