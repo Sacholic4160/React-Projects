7877c65e4bb266ef6af7551f81a5ba3510c40a58
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-27 11:56:57 +0530

    the Postcard component

--- 12MegaBlogProject/src/components/PostCard.jsx
+++ 12MegaBlogProject/src/components/PostCard.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import dbService from '../appwrite/config'
-import { Link } from 'react-router-dom'
+import appwriteService from "../appwrite/config"
+import {Link} from 'react-router-dom'
 
-function PostCard($id, title, featuredImage) {
+export default function PostCard({$id, title, featuredImage}) {
+    
   return (
-  <Link to={`/post/${$id}`}>
-    <div className='w-full bg-gray-100 rounded-xl p-4' >
-        <div className='w-full justify-centermb-4'>
-            <img src={dbService.getFilePreview(featuredImage)} alt={title}
-            className='rounded-xl' />
+    <Link to={`/post/${$id}`}>
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                className='rounded-xl' />
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
         </div>
-        <h2 
-        className='text-xl font-bold'>
-            {title}
-        </h2>
-    </div>
-  </Link>
+    </Link>
   )
 }
 
-export default PostCard
+
