3e9a42c487e293a8898d6a96d0a0643befe6d11e
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-27 12:08:15 +0530

    the store file

--- 12MegaBlogProject/src/store/store.js
+++ 12MegaBlogProject/src/store/store.js
@@ -1,7 +1,10 @@
 import {configureStore } from "@reduxjs/toolkit"
+import authSlice from "./authSlice";
 
 const store = new configureStore({
-    reducer: {}
+    reducer: {
+        auth : authSlice,
+    }
 })
 
 export default store;  
