0fe1ee8d165c277166562be67d038a257ea774ed
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-27 11:55:08 +0530

    here we are import everything of components folder and export from here

--- 12MegaBlogProject/src/components/index.js
+++ 12MegaBlogProject/src/components/index.js
@@ -3,6 +3,14 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Select from "./Select"
+import Login from "./Login";
+import Signup from "./Signup"
+import PostCard from "./PostCard"
+import Protected from "./AuthLayout";
+import PostForm from "./post-form/PostForm";
+import Button from "./Button"
+import Input from "./Input"
 
-
-export  { Header, Footer, Container, Logo, LogoutBtn  }
+export  {Input, Button, Header, Footer, Container, Logo, LogoutBtn, RTE, Select, Login, Signup, PostCard, Protected, PostForm }
